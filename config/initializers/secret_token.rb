b0f27ceeaacd69475f4727b4674bab66a6cfa2cc
Vince The Mac Vincent <mellterm@gmail.com>
2012-05-19 18:27:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termbase::Application.config.secret_token = '572dfae76659867d0da3f45edc8dc629c083dd13d11dac4f9ca3aa2a36affd7f8f322ffd52ec1fa02ff0d1bb3c22d265483f6496d70d6a23b2665af50bd569f1'
